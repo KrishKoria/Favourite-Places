2d17a9a6b07d5fa66499cb403d42d0d569a80e3f
KrishKoria <krishkoria2004@gmail.com>
2024-01-20 23:36:06 +0530

    changed add places to show an image picker area, created an image input widget

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favourite_places_app/providers/user_places.dart';
+import 'package:favourite_places_app/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -47,43 +48,23 @@ class _AddPlaceState extends ConsumerState<AddPlace> {
                         color: Theme.of(context).colorScheme.onBackground),
                   ),
                   const SizedBox(
-                    height: 16.0,
+                    height: 20.0,
                   ),
-                  Row(
-                    children: [
-                      Container(
-                        width: 100.0,
-                        height: 100.0,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 1.0,
-                            color: Theme.of(context).colorScheme.onBackground,
-                          ),
-                        ),
-                        child: const Center(
-                          child: Text('No Image'),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 16.0,
-                      ),
-                      Expanded(
-                        child: TextButton.icon(
-                          onPressed: () {},
-                          icon: const Icon(Icons.camera),
-                          label: const Text('Take Picture'),
-                        ),
-                      ),
-                    ],
-                  ),
+                  const ImageInput(),
                 ],
               ),
             ),
           ),
-          ElevatedButton.icon(
-            onPressed: _savePlace,
-            icon: const Icon(Icons.add),
-            label: const Text('Add Place'),
+          SizedBox(
+            width: 160,
+            child: ElevatedButton.icon(
+              onPressed: _savePlace,
+              icon: const Icon(Icons.add),
+              label: const Text(
+                'Add Place',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
           ),
         ],
       ),
